f5114f23b24d892713d7c4e528e01796bd55b9c3
paul.carranza@compass-usa.com <Paul.Carranza@compass-usa.com>
2018-11-15 12:06:02 -0600

    new project

---
+++ preDemo/preDemo/src/main/java/com/example/preDemo/BasicController.java
@@ -0,0 +1,13 @@
+package com.example.preDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+	@GetMapping("/hello")
+	public String helloCompass() {
+		System.out.println("Insied helloCompass method");
+		return "Hello Compass";
+	}
+}
---
+++ preDemo/preDemo/src/main/java/com/example/preDemo/PreDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.preDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreDemoApplication.class, args);
+	}
+}
