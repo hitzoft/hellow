93832b58b5af3001e94af1fa56ac05724dbd37c1
paul.carranza@compass-usa.com <Paul.Carranza@compass-usa.com>
2018-11-28 15:13:16 -0600

    ok

--- preDemo/preDemo/src/main/java/com/example/preDemo/BasicController.java
+++ preDemo/preDemo/src/main/java/com/example/preDemo/BasicController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
+	
+	
 	@GetMapping("/hello")
 	public String helloCompass() {
 		System.out.println("Insied helloCompass method");
 		return "Hello Compass 3";
 	}
+	
+	
+	
 }
