c4bf36eb06b03f90fa35495937083b363d5ae8dc
paul.carranza@compass-usa.com <Paul.Carranza@compass-usa.com>
2018-12-06 11:39:08 -0600

    build info

---
+++ preDemo/preDemo/buildInfo.bat
@@ -0,0 +1,11 @@
+cd preDemo/preDemo/
+docker build -t hello-compass:%BUILD_NUMBER% .
+docker tag hello-compass:%BUILD_NUMBER% hitzoft/hello-compass:%BUILD_NUMBER%
+docker images
+
+
+docker login -u hitzoft --password Zauds137*
+docker push hitzoft/hello-compass:%BUILD_NUMBER%
+
+kubectl set image deployment.v1.apps/hello-compass-deployment hello-compass=hitzoft/hello-compass:%BUILD_NUMBER% --record
+kubectl rollout status deployment.v1.apps/hello-compass-deployment
