bc85f1816e7e284f4475c3aafc36475e60583481
paul.carranza@compass-usa.com <Paul.Carranza@compass-usa.com>
2018-12-12 13:46:23 -0600

    gg

--- preDemo/preDemo/src/main/java/com/example/preDemo/BasicController.java
+++ preDemo/preDemo/src/main/java/com/example/preDemo/BasicController.java
@@ -1,18 +1,31 @@
 package com.example.preDemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
-	
-	
+
 	@GetMapping("/hello")
-	public String helloCompass() {
-		System.out.println("helloCompass method");
-		return "This message has been changed by Paul";
+	public String helloCompass() throws InterruptedException {
+		Thread.sleep(2000);
+		return "Hello Compass";
 	}
-	
-	
-	
+
+	@GetMapping("/bye")
+	public String byeCompass() {
+		return "Bye Compass";
+	}
+
+	@GetMapping("/morning")
+	public String morningCompass() {
+		return "Good Morning Compass";
+	}
+
+	@PostMapping("/myName")
+	public String myName(@RequestBody String myN) {
+		return "Your name is "+myN;
+	}
 }
